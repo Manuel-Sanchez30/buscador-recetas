411851d9d27c9b628234bee245d95dd2bc21960a
milton.sanchezrios88@gmail.com <milton.sanchezrios88@gmail.com>
2025-01-03 20:35:51 -0500

    Formulario creado

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import Header from './components/Header.vue';
+
 
 </script>
 
 <template>
-  
-  <h1 class="text-red-500 text-2xl font-bold">hello world</h1>
+
+  <Header/>
 
 </template>
 
