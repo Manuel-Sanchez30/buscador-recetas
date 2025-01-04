c1717387570cf640779b29ddadb0ef903732140a
milton.sanchezrios88@gmail.com <milton.sanchezrios88@gmail.com>
2025-01-03 22:10:53 -0500

    mejoras al componente Header, imagen de fondo, solo mostrar formulario en la vista inicio

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,14 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { computed } from 'vue';
+import { RouterLink, useRoute } from 'vue-router';
 
+const route = useRoute();
+
+const paginaInicio = computed(()=> route.name === 'inicio');
 
 </script>
 <template>
-    <header class="bg-slate-800">
+    <header class="bg-slate-800" :class="{header: paginaInicio}">
         <div class="mx-auto container px-5 py-16">
             <div class="flex justify-between items-center">
                 <div>
@@ -18,7 +22,7 @@ import { RouterLink } from 'vue-router';
                 </nav>
             </div>
             <div>
-                <form class="md:w-1/2 2xl:w-1/3 bg-orange-500 my-32 p-10 space-y-6 rounded-lg shadow-md">
+                <form v-if="paginaInicio" class="md:w-1/2 2xl:w-1/3 bg-orange-500 my-32 p-10 space-y-6 rounded-lg shadow-md">
                     <div class="space-y-4">
                         <label 
                             for="ingrediente"
@@ -54,5 +58,12 @@ import { RouterLink } from 'vue-router';
         </div>
     </header>
 </template>
+<style>
+    .header{
+        background-image: url('/img/bg.jpg');
+        background-size: cover;
+        background-position: center;
+    }
+</style>
 
 
