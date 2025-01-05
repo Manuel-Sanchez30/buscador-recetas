ed3711320028c3fbf0465b780ff3ef86c2ffa613
milton.sanchezrios88@gmail.com <milton.sanchezrios88@gmail.com>
2025-01-05 18:16:02 -0500

    se guarda favoritos en localStorage

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { useBebidasStore } from "./bebidas";
-import { ref } from "vue";
+import { ref, watch, onMounted } from "vue";
 
 
 
@@ -9,14 +9,40 @@ export const useFavoritosStore = defineStore('favoritos', ()=>{
     const favoritos = ref([]);
     const bebidas = useBebidasStore();
 
+    onMounted(()=>{
+        favoritos.value = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+    });
+
+    watch(favoritos, ()=>{
+        sincronizarLocalStore()
+    },{
+        deep: true,
+    });
+
     function handleClickFavorito(){
-        favoritos.value.push(bebidas.receta);
-        console.log(favoritos);
-    }
+        if(existeFavorito(bebidas.receta.idDrink)){
+            console.log('ya existe...');
+        }else{
+            favoritos.value.push(bebidas.receta);
+        }
+    };
+
+    // almacenar en localStorage
+    function sincronizarLocalStore(){
+        localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
+    };
+
+    //evitar elementos duplicados
+    function existeFavorito(id){
+        const favoritoLocalStorage = JSON.parse(localStorage.getItem('favoritos')) ?? [];
+        return favoritoLocalStorage.some(favorito => favorito.idDrink === id)
+    };
 
     return {
         favoritos,
-        handleClickFavorito
+        handleClickFavorito,
+        existeFavorito
+
 
     }
 
--- src/views/FavoritosView.vue
+++ src/views/FavoritosView.vue
@@ -1,8 +1,21 @@
 <script setup>
+import Receta from "@/components/Receta.vue";
+import { useFavoritosStore } from "@/stores/favoritos";
+
+
+    const favoritos = useFavoritosStore();
+
 
 </script>
 <template>
-    <h1>Favotiros</h1>
+    <h1 class="text-3xl md:text-6xl font-bold text-center mt-3">Favotiros</h1>
+    <div class="container mx-auto grid grid-cols-3 md:grid-cols-4 mt-5 md:mt-10 gap-3 px-2">
+        <Receta
+            v-for="receta in favoritos.favoritos"
+            :key="receta.idDrink"
+            :receta="receta"
+        />
+    </div>
 </template>
 
 
