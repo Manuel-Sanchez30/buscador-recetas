0b7c7ff174f449f85dfcb91fdaef1c445d8c58c1
milton.sanchezrios88@gmail.com <milton.sanchezrios88@gmail.com>
2025-01-05 11:39:37 -0500

    se crea el servicio buscarRecetas

--- src/services/APIServices.js
+++ src/services/APIServices.js
@@ -3,6 +3,9 @@ import api from '../services/lib/axios';
 export default {
     obtenerCategorias(){
         return api.get('/list.php?c=list')
+    },
+    buscarRecetas({categoria, nombre}){
+        return api.get(`/filter.php?c=${categoria}&i=${nombre}`)
     }
 }
 
